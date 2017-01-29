17c2890f6f9acb7b3f80dc67e341712571d4e20b
Christian Hauptmann <Hauptmann-ch@web.de>
2017-01-29 16:49:58 +0100

    MongoDB URLs
    urls für einfügen, bearbeiten,... hinzugefügt

--- expressjs_server/server.js
+++ expressjs_server/server.js
@@ -9,8 +9,31 @@ app.get('/',function(request,response){
     response.status(200).send('Hello World!');
 });
 
-app.get('/test',function(request,response){
-    response.status(200).send('1 + 1 = 2');
+app.get('/rechnung',function(request,response){
+    var a = 1;
+    var b = 2;
+    var c = a + b;
+    response.status(200).send(a + ' + ' + b + ' = ' + c);
+});
+
+app.get('/insert',function(request,response){
+    response.status(200).send('insert');
+});
+
+app.get('/delete',function(request,response){
+    response.status(200).send('delete');
+});
+
+app.get('/update',function(request,response){
+    response.status(200).send('update');
+});
+
+app.get('/search',function(request,response){
+    response.status(200).send('search');
+});
+
+app.get('/searchAll',function(request,response){
+    response.status(200).send('searchAll');
 });
 
 app.listen(PORT,function(){
